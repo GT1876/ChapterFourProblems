387a6269bbb5856d69f462c92529601ea24e4210
GT1876 <garrysontime@gmail.com>
2014-03-26 09:42:06 -0400

    Big Turd

---
+++ ChapterFourProblems/EnsureValidPayRateLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnsureValidPayRateLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double n;
+            while (true)
+            {
+                Console.WriteLine("Enter a Hourly Pay Rate");
+                {
+                    input = Console.ReadLine();
+                    n = Convert.ToDouble(input);
+
+                   
+                }
+                if (n < 5.65 || n > 49.99)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ ChapterFourProblems/GetVowel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while (true)
+            {
+                Console.WriteLine("Enter a vowel");
+                input = Console.ReadLine().ToLower();
+
+                if (input == "a" || input == "e" || input == "i" || input == "o" || input == "u")
+                {
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+                    Console.WriteLine("Not a Vowel");
+                }
+                if (input == "!")
+                {
+                    break;
+                }
+                    
+            }
+        }
+    }
+}
